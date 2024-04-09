d8f6cc064d11a8d0db45e0e3e855a25af02dac6b
Siarheispivak <siarheispivak2@gmail.com>
2024-04-09 14:01:07 +0300

    fix: classNames were replaced with clsx.

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,19 +1,20 @@
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
 
-import s from './button.module.scss'
+import s from "./button.module.scss";
+import clsx from "clsx";
 
-export type ButtonProps<T extends ElementType = 'button'> = {
+export type ButtonProps<T extends ElementType = "button"> = {
   as?: T
   children: ReactNode
   className?: string
   fullWidth?: boolean
-  variant?: 'primary' | 'secondary'
+  variant?: "primary" | "secondary"
 } & ComponentPropsWithoutRef<T>
 
-export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
-
+export const Button = <T extends ElementType = "button">(props: ButtonProps<T>) => {
+  const { as: Component = "button", className, fullWidth, variant = "primary", ...rest } = props;
+  const classNames = clsx(`${s[variant]} ${fullWidth ? s.fullWidth : ""} ${className}`);
   return (
-    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
-  )
-}
+    <Component className={classNames} {...rest} />
+  );
+};
