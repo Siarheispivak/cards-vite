9299a6f3aa514b42ac61984aa293a82ec5cf5989
Siarheispivak <siarheispivak2@gmail.com>
2024-04-10 00:03:16 +0300

    fix: clsx class assignment method has been changed.

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,20 +1,20 @@
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 
-import s from "./button.module.scss";
-import clsx from "clsx";
+import clsx from 'clsx'
 
-export type ButtonProps<T extends ElementType = "button"> = {
+import s from './button.module.scss'
+
+export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
   children: ReactNode
   className?: string
   fullWidth?: boolean
-  variant?: "primary" | "secondary"
+  variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
-export const Button = <T extends ElementType = "button">(props: ButtonProps<T>) => {
-  const { as: Component = "button", className, fullWidth, variant = "primary", ...rest } = props;
-  const classNames = clsx(`${s[variant]} ${fullWidth ? s.fullWidth : ""} ${className}`);
-  return (
-    <Component className={classNames} {...rest} />
-  );
-};
+export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+  const classNames = clsx(s[variant], fullWidth && s.fullWidth, className)
+
+  return <Component className={classNames} {...rest} />
+}
