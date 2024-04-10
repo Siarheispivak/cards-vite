82ec808f69c9312a2f9e1e4befbecfc8e45fb3d2
Siarheispivak <siarheispivak2@gmail.com>
2024-04-10 14:41:39 +0300

    feat: svgComponents were added.

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as CloseIcon } from './close'
+export { default as IconClose } from './close'
+export { default as Eye } from './eye'
+export { default as VisibilityOff } from './visibility-off'
