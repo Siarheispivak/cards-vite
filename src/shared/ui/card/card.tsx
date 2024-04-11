c01ab45c4af22182258b6197de089f6e960209f3
Siarheispivak <siarheispivak2@gmail.com>
2024-04-11 11:14:33 +0300

    create:files for card component were added.

---
+++ src/shared/ui/card/card.tsx
@@ -0,0 +1,5 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+export type CardProps = ComponentPropsWithoutRef<'div'>
+
+// export const Card = forwardRef<ElementRef<'div'>, CardProps>((props, ref) => {})
