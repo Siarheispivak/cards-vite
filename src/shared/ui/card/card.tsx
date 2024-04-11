ecf605fc8b4dc715697ac498d763ca0ebc1a59e3
Siarheispivak <siarheispivak2@gmail.com>
2024-04-11 13:20:36 +0300

    feat: card component was finished, story and styles were added.

--- src/shared/ui/card/card.tsx
+++ src/shared/ui/card/card.tsx
@@ -1,5 +1,9 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+import s from './card.module.scss'
 
 export type CardProps = ComponentPropsWithoutRef<'div'>
 
-// export const Card = forwardRef<ElementRef<'div'>, CardProps>((props, ref) => {})
+export const Card = forwardRef<ElementRef<'div'>, CardProps>(({ className, ...restProps }, ref) => {
+  return <div className={s.root} ref={ref} {...restProps}></div>
+})
