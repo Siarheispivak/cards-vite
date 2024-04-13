5461e9c94b91003da5fb765c5f0dd97c20e6de27
Siarheispivak <siarheispivak2@gmail.com>
2024-04-13 20:15:50 +0300

    create: RadixCheckbox was added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { TextField } from '@/shared/ui/text-field'
+import { useState } from 'react'
+
+import { Checkbox } from '@/shared/ui/checkbox'
+
+import s from './shared/ui/checkbox/checkbox.module.scss'
 
 export function App() {
+  const [checked, setChecked] = useState(false)
+
+  const onValueChange = () => {
+    setChecked(!checked)
+  }
+
+  console.log(checked)
+
   return (
     <div>
-      <TextField type={'search'} />
+      <Checkbox
+        checked={checked}
+        className={s.checkbox}
+        disabled={false}
+        id={'345bgv'}
+        onValueChange={onValueChange}
+        position={'left'}
+      />
     </div>
   )
 }
