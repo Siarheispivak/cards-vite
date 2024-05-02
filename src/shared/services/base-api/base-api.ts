6fc1bae81fe98d8658de4220c3efbbfbad7f038f
Siarheispivak <siarheispivak2@gmail.com>
2024-05-02 23:18:49 +0300

    create: baseApi was created.

---
+++ src/shared/services/base-api/base-api.ts
@@ -0,0 +1,10 @@
+import { baseQueryWithReauth } from '@/shared/services/base-api/base-query-with-reauth'
+import { createApi } from '@reduxjs/toolkit/query/react'
+
+export const baseApi = createApi({
+  baseQuery: baseQueryWithReauth,
+  // refetchOnFocus: true,
+  endpoints: () => ({}),
+  reducerPath: 'baseApi',
+  tagTypes: ['AuthMe'],
+})
