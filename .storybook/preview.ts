e44be115a10fdf5c90da5c06c87f4e933fec6fc9
Siarheispivak <siarheispivak2@gmail.com>
2024-05-09 22:11:59 +0300

    feat: storybook-addon-react-router-v6 was added.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/react'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '../src/app/styles/index.scss'
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 const preview: Preview = {
   parameters: {
@@ -13,5 +14,5 @@ const preview: Preview = {
     },
   },
 }
-
+export const decorators = [withRouter]
 export default preview
