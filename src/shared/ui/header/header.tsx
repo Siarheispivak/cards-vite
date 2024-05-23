c6ed09402b60cad5cf68a28279bee5f6a0dc81ad
Siarheispivak <siarheispivak2@gmail.com>
2024-05-23 23:37:25 +0300

    feat:typography was added into the button.

--- src/shared/ui/header/header.tsx
+++ src/shared/ui/header/header.tsx
@@ -1,6 +1,9 @@
+import { Link } from 'react-router-dom'
+
 import { LogoIncubator } from '@/shared/assets'
+import { routes } from '@/shared/const'
 import { useAuthMeQuery } from '@/shared/services/auth-api/auth-api'
-import { Button } from '@/shared/ui'
+import { Button, Typography } from '@/shared/ui'
 import { MenuHeader } from '@/shared/ui/menu-header'
 
 import s from './header.module.scss'
@@ -16,7 +19,13 @@ export const Header = () => {
     <header className={s.header}>
       <LogoIncubator />
       {data && <MenuHeader email={data.email} name={data.name} src={data.avatar} />}
-      {!data && <Button variant={'primary'}>Sign In</Button>}
+      {!data && (
+        <Button variant={'primary'}>
+          <Typography as={Link} className={s.signIn} to={routes.AUTH.SING_IN} variant={'link1'}>
+            Sign In
+          </Typography>
+        </Button>
+      )}
     </header>
   )
 }
