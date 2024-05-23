90b338efe9afb1ca638a04d16eedce3d42550b7f
Siarheispivak <siarheispivak2@gmail.com>
2024-05-23 17:31:42 +0300

    fix:paths has been changed.

--- src/shared/ui/index.tsx
+++ src/shared/ui/index.tsx
@@ -1,17 +1,26 @@
-export * from './drop-down'
-export * from './table'
-export * from './avatar'
+export { Avatar } from './avatar'
 export { Button } from './button'
-export { Header } from './header'
-export { MenuHeader } from './menu-header'
-export * from './radio-group'
-export * from './controlled'
 export type { ButtonProps } from './button'
 export { Card } from './card'
 export type { CardProps } from './card'
 export { Checkbox } from './checkbox'
 export type { CheckboxProps } from './checkbox'
+export { ControlledCheckbox, ControlledRadioGroup, ControlledTextField } from './controlled'
+export { Dropdown, DropdownItem, DropdownItemWithIcon } from './drop-down'
+export { Header } from './header'
+export { MenuHeader } from './menu-header'
+export { RadioGroup } from './radio-group'
 export { Select } from './select'
+export {
+  HeaderTable,
+  Table,
+  TableBody,
+  TableCell,
+  TableEmpty,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+} from './table'
 export { TextField } from './text-field'
 export type { TextFieldProps } from './text-field'
 export { Typography } from './typography'
