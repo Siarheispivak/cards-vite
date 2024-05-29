a19021e8b417e3eb9bc73c4ce0b0ca0836036b43
Siarheispivak <siarheispivak2@gmail.com>
2024-05-29 23:36:40 +0300

    create: use-action, use-app-dispatch, use-app-selector, cutStringParams, use-Debounce hooks were created.

---
+++ src/shared/lib/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useActions } from './use-action'
+export { useAppDispatch } from './use-app-dispatch'
+export { useAppSelector } from './use-app-selector'
+export { useDebounce } from './use-debounce'
