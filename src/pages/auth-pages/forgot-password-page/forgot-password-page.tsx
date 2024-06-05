ba0b20470c1251002ae9b1a17cd547ee98b2f957
Siarheispivak <siarheispivak2@gmail.com>
2024-06-05 17:49:11 +0300

    fix:props was changed.

--- src/pages/auth-pages/forgot-password-page/forgot-password-page.tsx
+++ src/pages/auth-pages/forgot-password-page/forgot-password-page.tsx
@@ -2,17 +2,21 @@ import { useNavigate } from 'react-router-dom'
 
 import { appAction } from '@/app/modal'
 import { ForgotPasswordForm } from '@/features/auth'
+import { emailMessage } from '@/pages/auth-pages/forgot-password-page/email-message'
 import { routes } from '@/shared/const'
 import { useActions } from '@/shared/lib'
-import { useRecoveryPasswordMutation } from '@/shared/services'
+import { VerificationEmailArgs, useRecoveryPasswordMutation } from '@/shared/services'
 
 export const ForgotPasswordPage = () => {
   const [recoveryPassword] = useRecoveryPasswordMutation()
   const { setEmail } = useActions(appAction)
   const navigate = useNavigate()
-  const handleResetPassword = async (args: { email: string }) => {
+  const handleResetPassword = async (args: VerificationEmailArgs) => {
     try {
-      await recoveryPassword(args)
+      await recoveryPassword({
+        email: args.email,
+        html: emailMessage,
+      })
       setEmail(args)
       navigate(routes.AUTH.CHECK_EMAIL)
     } catch (e) {
