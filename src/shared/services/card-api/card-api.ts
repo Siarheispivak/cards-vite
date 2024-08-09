8a3739dcb439386b0928f88e59db3dbcada6017e
Siarheispivak <siarheispivak2@gmail.com>
2024-08-09 18:24:47 +0300

    feat: CRUD operations has been added to cards-api.

--- src/shared/services/card-api/card-api.ts
+++ src/shared/services/card-api/card-api.ts
@@ -2,10 +2,13 @@ import { baseApi } from '@/shared/services'
 
 import {
   CreateLearnGradeArgs,
+  GetCardByIdResponse,
   GetDeckArgs,
   GetDeckResponse,
   GetLearnGradeArgs,
   LearnResponse,
+  retrieveCardsArgs,
+  retrieveCardsInDeck,
 } from './card.types'
 
 export const cardApi = baseApi.injectEndpoints({
@@ -19,6 +22,21 @@ export const cardApi = baseApi.injectEndpoints({
           url: `v1/decks/${args.cardId}/learn`,
         }),
       }),
+      creteCard: builder.mutation<GetCardByIdResponse, { data: FormData; id: string }>({
+        invalidatesTags: ['Cards'],
+        query: args => ({
+          body: args.data,
+          method: 'POST',
+          url: `v1/decks/${args.id}/cards`,
+        }),
+      }),
+      deleteCard: builder.mutation<void, { id: string }>({
+        invalidatesTags: ['Cards'],
+        query: args => ({
+          method: 'DELETE',
+          url: `v1/cards/${args.id}`,
+        }),
+      }),
       getDeck: builder.query<GetDeckResponse, GetDeckArgs>({
         query: args => ({
           url: `v1/decks/${args.id}`,
@@ -30,8 +48,25 @@ export const cardApi = baseApi.injectEndpoints({
           url: `v1/decks/${args.id}/learn`,
         }),
       }),
+      retrieveCards: builder.query<retrieveCardsInDeck, retrieveCardsArgs>({
+        providesTags: ['Cards'],
+        query: ({ id, ...args }) => ({
+          body: args ?? undefined,
+          url: `/v1/cards/${id}/cards`,
+        }),
+      }),
+      updateCard: builder.mutation<GetCardByIdResponse, { data: FormData; id: string | undefined }>(
+        {
+          invalidatesTags: ['Cards'],
+          query: args => ({
+            body: args.data,
+            method: 'PATCH',
+            url: `v1/cards/${args.id}`,
+          }),
+        }
+      ),
     }
   },
 })
 
-export const { useCreateLearnGradeMutation, useGetDeckQuery, useGetLearnCardQuery } = cardApi
+export const { useGetDeckQuery, useGetLearnCardQuery } = cardApi
